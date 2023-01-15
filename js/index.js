bb187975498f2575893841ad352b79fbe4c4db0d
minderers <2210151384@qq.com>
2023-01-16 01:39:39 +0800

    新上好课和左广告栏

--- js/index.js
+++ js/index.js
@@ -242,3 +242,23 @@ setInterval(() => {
         }, 10);
     }
 }
+
+// 课程切换
+// 获取所有的a标签（ tab 栏)
+const tabs = document.querySelectorAll('.new-course-box a');
+// 获取到所有的课程列表
+const uls = document.querySelectorAll('.new-course-list ul');
+
+for (let i = 0; i < tabs.length; i++) {
+    // 循环为所有的a绑定点击事件
+    tabs[i].onclick = function () {
+        //清除所有的a 和ul的样式
+        for (let j = 0; j < tabs.length; j++) {
+            tabs[j].className = '';
+            uls[j].className = '';
+        }
+        //给当前选中的a和ul 添加样式
+        tabs[i].className = 'active';
+        uls[i].className = 'current';
+    }
+}
